bb45bf6cb3fa4b20a3baf437a6bb31dea31edeeb
Georgios Papadopoulos <georgiospapadopoulos@quantexa.com>
2025-09-15 18:08:14 +0200

    Initial routing implemented, header, footer, and scss global vars and mixins and fonts and color pallette

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ResultsComponent } from './pages/results/results.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  {
+    path: 'results',
+    component: ResultsComponent,
+    title: 'Results',
+  },
+  {
+    path: 'details/:gameId',
+    component: DetailsComponent,
+    title: 'Details',
+  },
+  {
+    path: 'checkout',
+    component: CheckoutComponent,
+    title: 'Checkout',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
