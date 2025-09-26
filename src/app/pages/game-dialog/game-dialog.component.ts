de5c0913c1abd3d5a8b165c0389852f17c22d354
Georgios Papadopoulos <georgiospapadopoulos@quantexa.com>
2025-09-26 16:18:41 +0200

    Initial implementation of the game dialog with the accordion and the first row implemented

--- src/app/pages/game-dialog/game-dialog.component.ts
+++ src/app/pages/game-dialog/game-dialog.component.ts
@@ -1,7 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { Dialog } from 'primeng/dialog';
+import { AccordionModule } from 'primeng/accordion';
+import { ChipModule } from 'primeng/chip';
+import { TagModule } from 'primeng/tag';
+import { Ripple, RippleModule } from 'primeng/ripple';
 import { Store } from '@ngrx/store';
 import { getSelectedGame } from '../../store/cart/cart.selectors';
 import { setSelectedGame } from '../../store/cart/cart.actions';
@@ -10,13 +21,24 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-game-dialog',
   standalone: true,
-  imports: [Dialog, ButtonModule, InputTextModule],
+  imports: [
+    CommonModule,
+    Dialog,
+    ButtonModule,
+    InputTextModule,
+    AccordionModule,
+    ChipModule,
+    TagModule,
+    RippleModule,
+  ],
   templateUrl: './game-dialog.component.html',
   styleUrl: './game-dialog.component.scss',
 })
-export class GameDialogComponent implements OnInit {
+export class GameDialogComponent implements OnInit, AfterViewInit {
   visible: boolean = false;
   selectedGame: any;
+  @ViewChild('rippleDiv', { read: Ripple }) ripple!: Ripple;
+  @ViewChild('rippleDiv') rippleDiv!: ElementRef<HTMLDivElement>;
 
   constructor(
     private store: Store,
@@ -29,6 +51,34 @@ export class GameDialogComponent implements OnInit {
       this.visible = !!game;
       this.selectedGame = game;
     });
+  }
+
+  ngAfterViewInit() {
+    this.triggerCornerRipples();
+  }
+
+  triggerCornerRipples() {
+    const div = this.rippleDiv.nativeElement;
+    const corners = [
+      { x: 0, y: 0 }, // top-left
+      { x: div.offsetWidth, y: 0 }, // top-right
+      { x: div.offsetWidth, y: div.offsetHeight }, // bottom-right
+      { x: 0, y: div.offsetHeight }, // bottom-left
+    ];
+
+    let i = 0;
+    setInterval(() => {
+      const { x, y } = corners[i % 4];
+      const event = new MouseEvent('mousedown', {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+        clientX: x + div.getBoundingClientRect().left,
+        clientY: y + div.getBoundingClientRect().top,
+      });
+      div.dispatchEvent(event);
+      i++;
+    }, 1000);
   }
 
   onCloseDialog() {
